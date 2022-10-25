0590ab06d2c0cab1582eb7325014ef15cc0e63d3
minoolian <mw.lee@kakao.com>
2022-10-25 13:49:38 +0900

    feat: Multimodule Setup

---
+++ backend/study/src/main/java/com/wayfarer/study/service/StudyService.java
@@ -0,0 +1,11 @@
+package com.wayfarer.study.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudyService {
+
+    public String test() {
+        return "test";
+    }
+}
