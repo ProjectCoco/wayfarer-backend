d0234444f372ac85fe9318f19f34c152b78e87d8
5witchkr <hob4419@gmail.com>
2022-10-25 14:22:49 +0900

    feat: test example

---
+++ backend/study/src/test/java/com/wayfarer/study/StudyArticleDomainTest.java
@@ -0,0 +1,11 @@
+package com.wayfarer.study;
+
+public class StudyArticleDomainTest {
+
+
+    var = "123";
+
+    StudyArticle studyArticle = studyArticle.builder().title(title);
+
+    assertThat(studyArticle.gettitle, title);
+}
