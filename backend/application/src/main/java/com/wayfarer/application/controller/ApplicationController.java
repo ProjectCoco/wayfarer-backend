90242d1402f52ae4d8d1f6b2d751e2e111c33b78
5witchkr <hob4419@gmail.com>
2022-11-01 13:04:52 +0900

    feat: study controller

--- backend/application/src/main/java/com/wayfarer/application/controller/ApplicationController.java
+++ backend/application/src/main/java/com/wayfarer/application/controller/ApplicationController.java
@@ -6,9 +6,7 @@ import com.wayfarer.study.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +18,30 @@ public class ApplicationController {
     private final StudyService studyService;
 
     @GetMapping("")
+    //todo page
     public ResponseEntity<List<StudyResponseDto>> readAllStudy() {
         return new ResponseEntity<>(studyService.readAllStudyArticles(), HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity readStudy() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity createStudy() {
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{target}")
+    public ResponseEntity updateStudy(@PathVariable String target) {
+        //target -> info, title, tag, time, contents, ....
+        return new ResponseEntity(HttpStatus.RESET_CONTENT);
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity deleteSutdy() {
+        //delete -> status 변경
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- backend/study/src/main/java/com/wayfarer/study/service/StudyServiceMockImpl.java
+++ backend/study/src/main/java/com/wayfarer/study/service/StudyServiceMockImpl.java
@@ -1,11 +1,8 @@
 package com.wayfarer.study.service;
 
-import com.wayfarer.study.dto.StudyArticleResponseDto;
 import com.wayfarer.study.dto.StudyResponseDto;
-import com.wayfarer.study.entity.StudyArticle;
 import com.wayfarer.study.helper.StudyStubData;
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
