5085c63ff2abf90a61a0516284d9581ae76a71f5
minoolian <mw.lee@kakao.com>
2022-12-14 17:23:12 +0900

    feat(project): 사이드프로젝트 Service 인터페이스 정의
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/project/src/main/java/com/wayfarer/project/service/ProjectArticleService.java
+++ backend/project/src/main/java/com/wayfarer/project/service/ProjectArticleService.java
@@ -2,4 +2,13 @@ package com.wayfarer.project.service;
 
 public interface ProjectArticleService {
 
+    void createProjectArticle(ProjectArticleRequestDto projectArticleRequestDto);
+
+    MultiResponseDto<ProjectArticleDetailResponseDto> readProjectArticle(int page);
+
+    void updateProjectArticle(Long projectId, ProjectArticleUpdateRequestDto projectArticleUpdateRequestDto);
+
+    void deleteProjectArticle(Long projectId);
+
+
 }
