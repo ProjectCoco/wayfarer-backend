02fea0ced32b9739188b247ff024d2d0434a4804
minoolian <mw.lee@kakao.com>
2022-12-15 16:45:40 +0900

    feat(project): 사이드프로젝트 조회 쿼리 추가
    * 전체 게시글 조회 쿼리 추가
    
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/project/src/main/java/com/wayfarer/project/repository/ProjectArticleRepository.java
+++ backend/project/src/main/java/com/wayfarer/project/repository/ProjectArticleRepository.java
@@ -1,7 +1,11 @@
 package com.wayfarer.project.repository;
 
 import com.wayfarer.project.entity.ProjectArticle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectArticleRepository extends JpaRepository<ProjectArticle, Long> {
+
+    Page<ProjectArticle> findByEnabled(Boolean enabled, Pageable pageable);
 }
