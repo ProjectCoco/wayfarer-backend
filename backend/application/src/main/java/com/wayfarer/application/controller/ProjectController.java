d7f8b23fd2cce5176c9a46e514d0896e582ee968
minoolian <mw.lee@kakao.com>
2022-12-22 11:30:23 +0900

    fix(project): 핸들러 메서드 수정
    * CRUD 핸들러 메서드의 파라미터 정리
    
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/application/src/main/java/com/wayfarer/application/controller/ProjectController.java
+++ backend/application/src/main/java/com/wayfarer/application/controller/ProjectController.java
@@ -1,7 +1,8 @@
 package com.wayfarer.application.controller;
 
 
-import com.wayfarer.study.dto.StudyArticleUpdateRequestDto;
+import com.wayfarer.project.dto.ProjectArticleRequestDto;
+import com.wayfarer.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,27 +17,28 @@ public class ProjectController {
 
     @GetMapping("")
     //todo page
-    public ResponseEntity readAllProject(@RequestParam int page) {
-        return new ResponseEntity<>(projectService.readAllProjectArticles(page), HttpStatus.OK);
+    public ResponseEntity readAllProject(@RequestParam int page,
+                                         @RequestParam(defaultValue = "true") Boolean status) {
+        return new ResponseEntity<>(projectService.readAllProjectArticles(page, status), HttpStatus.OK);
     }
 
-    @GetMapping("/tag")
-    public ResponseEntity readProjectWithTag(@RequestParam int page, String tag) {
+    /*@GetMapping("/tag")
+    public ResponseEntity readProjectWithTag(@RequestParam int page,
+                                             @RequestParam(required = true) String tag,
+                                             @RequestParam(defaultValue = "true") Boolean status) {
         return new ResponseEntity<>(projectService.readProjectArticlesWithTag(page, tag), HttpStatus.OK);
-    }
+    }*/
 
     @GetMapping("/{projectId}")
     public ResponseEntity readStudy(@PathVariable Long projectId) {
-        return new ResponseEntity<>(projectService.readProjectAritcle(projectId), HttpStatus.OK);
+        return new ResponseEntity<>(projectService.readProjectArticle(projectId), HttpStatus.OK);
     }
 
     @PostMapping("")
-    public ResponseEntity createProject(@RequestBody ProjectAritcleRequestDto projectAritcleRequestDto) {
+    public ResponseEntity createProject(@RequestBody ProjectArticleRequestDto projectAritcleRequestDto) {
         projectService.createProjectArticle(projectAritcleRequestDto);
         return new ResponseEntity(HttpStatus.CREATED);
     }
-
-
 
 
     @DeleteMapping("/{projectId}")
