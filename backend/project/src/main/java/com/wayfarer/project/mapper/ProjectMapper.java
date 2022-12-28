32ae69bd60f943ee309d44307b789671c810ab40
minoolian <mw.lee@kakao.com>
2022-12-28 10:27:16 +0900

    feat(project): Mapper 상호간 필드 mapping 추가
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/project/src/main/java/com/wayfarer/project/mapper/ProjectMapper.java
+++ backend/project/src/main/java/com/wayfarer/project/mapper/ProjectMapper.java
@@ -5,14 +5,31 @@ import com.wayfarer.project.dto.ProjectArticleRequestDto;
 import com.wayfarer.project.dto.ProjectArticleResponseDto;
 import com.wayfarer.project.entity.ProjectArticle;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProjectMapper {
 
+    @Mapping(source = "projectSkill", target = "projectSkill.skillId")
+    @Mapping(source = "content", target = "projectContent.content")
+    @Mapping(source = "startTime", target = "projectTime.startTime")
+    @Mapping(source = "totalMember", target = "projectMember.totalMember")
     ProjectArticle projectRequestDtoToProjectArticle(ProjectArticleRequestDto projectArticleRequestDto);
 
+    @Mapping(source = "projectContent.content", target = "content")
+    @Mapping(source = "projectInfo.status", target = "status")
+    @Mapping(source = "projectTime.startTime", target = "startTime")
+    @Mapping(source = "projectMember.totalMember", target = "totalMember")
+    @Mapping(source = "projectMember.countMember", target = "countMember")
+    ProjectArticleResponseDto projectArticleToProjectArticleResponseDto(ProjectArticle projectArticle);
+
+    @Mapping(source = "projectContent.content", target = "content")
+    @Mapping(source = "projectInfo.status", target = "status")
+    @Mapping(source = "projectTime.startTime", target = "startTime")
+    @Mapping(source = "projectMember.totalMember", target = "totalMember")
+    @Mapping(source = "projectMember.countMember", target = "countMember")
     ProjectArticleDetailResponseDto projectArticleToProjectArticleDetailResponseDto(ProjectArticle projectArticle);
 
     List<ProjectArticleResponseDto> projectArticleListToProjectArticleResponseDtoList(List<ProjectArticle> content);
