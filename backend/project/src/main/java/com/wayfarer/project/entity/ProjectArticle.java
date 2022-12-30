9065cb1b47d452ca0bb61a1e5a046a2c109e43d9
minoolian <mw.lee@kakao.com>
2022-12-31 00:31:26 +0900

    feat(proejct): JPA Auditing 적용
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/project/src/main/java/com/wayfarer/project/entity/ProjectArticle.java
+++ backend/project/src/main/java/com/wayfarer/project/entity/ProjectArticle.java
@@ -13,6 +13,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -26,6 +27,7 @@ import java.util.stream.Collectors;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class ProjectArticle {
 
     @Id
