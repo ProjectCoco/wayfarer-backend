aa35450687e96550cbc6b4df0395f37c17aa3a41
minoolian <mw.lee@kakao.com>
2023-01-03 14:12:10 +0900

    fix(study): JPA Auditing 중복 제거
    * 기존에 project 모듈에서 Auditing 활성
    
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/study/src/main/java/com/wayfarer/study/config/JpaAuditingConfig.java
+++ backend/study/src/main/java/com/wayfarer/study/config/JpaAuditingConfig.java
@@ -3,7 +3,7 @@ package com.wayfarer.study.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Configuration
-@EnableJpaAuditing
+//@Configuration
+//@EnableJpaAuditing
 public class JpaAuditingConfig {
 }
