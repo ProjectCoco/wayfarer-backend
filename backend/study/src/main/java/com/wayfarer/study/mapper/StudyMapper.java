f39e729ec244b4952dc0f1780e75c128739f0fa8
minoolian <mw.lee@kakao.com>
2023-01-03 21:51:22 +0900

    fix(study): Mapper Mapping 변경(정리)
    * 삭제된 필드 및 Member 관련 Mapping 수정
    
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/study/src/main/java/com/wayfarer/study/mapper/StudyMapper.java
+++ backend/study/src/main/java/com/wayfarer/study/mapper/StudyMapper.java
@@ -1,41 +1,27 @@
 package com.wayfarer.study.mapper;
 
-import com.wayfarer.study.dto.StudyArticleDetailResponseDto;
-import com.wayfarer.study.dto.StudyArticleRequestDto;
-import com.wayfarer.study.dto.StudyArticleResponseDto;
+import com.wayfarer.study.dto.*;
 import com.wayfarer.study.entity.StudyArticle;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface StudyMapper {
 
-    @Mapping(source = "studyContent.content", target = "content")
-    @Mapping(source = "studyInfo.status", target = "status")
-    @Mapping(source = "studyTime.startTime", target = "startTime")
-    @Mapping(source = "studyTime.deadline", target = "deadline")
-    @Mapping(source = "studyMember.totalMember", target = "totalMember")
-    @Mapping(source = "studyMember.countMember", target = "countMember")
-    StudyArticleDetailResponseDto studyArticleToStudyDetailResponseDto(StudyArticle studyArticle);
+    @Mapping(source = "studyArticle.studyContent.content", target = "content")
+    @Mapping(source = "studyArticle.studyInfo.status", target = "status")
+    @Mapping(source = "studyArticle.studyTime.startTime", target = "startTime")
+    StudyArticleDetailResponseDto studyArticleToStudyDetailResponseDto(StudyArticle studyArticle, List<StudyMemberResponseDto> studyMemberResponseDtos);
 
-    @Mapping(source = "studyInfo.status", target = "status")
-    @Mapping(source = "studyTime.startTime", target = "startTime")
-    @Mapping(source = "studyTime.deadline", target = "deadline")
-    @Mapping(source = "studyMember.totalMember", target = "totalMember")
-    @Mapping(source = "studyMember.countMember", target = "countMember")
-    @Mapping(source = "studyPosition.positionName", target = "positionName")
-    StudyArticleResponseDto studyArticleToStudyResponseDto(StudyArticle studyArticle);
+    @Mapping(source = "studyArticle.studyInfo.status", target = "status")
+    StudyArticleResponseDto studyArticleToStudyResponseDto(StudyArticle studyArticle, List<StudyMemberResponseDto> studyMemberResponses);
 
     List<StudyArticleResponseDto> studyArticleListToStudyArticleResponseDtoList(List<StudyArticle> studyArticleList);
 
     @Mapping(source = "content", target = "studyContent.content")
     @Mapping(source = "startTime", target = "studyTime.startTime")
-    @Mapping(source = "deadline", target = "studyTime.deadline")
-    @Mapping(source = "totalMember", target = "studyMember.totalMember")
-    @Mapping(source = "positionName", target = "studyPosition.positionName")
     StudyArticle studyRequestDtoToStudyArticle(StudyArticleRequestDto studyArticleRequestDto);
 
 
