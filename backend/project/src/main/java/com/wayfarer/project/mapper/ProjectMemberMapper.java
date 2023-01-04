42330e6c0a5bc9f317c513265e980cb0d6a1e75c
minoolian <mw.lee@kakao.com>
2023-01-04 15:07:52 +0900

    feat(project): Member Mapper 추가
    Co-authored-by: 5witchkr <hob4419@gmail.com>

---
+++ backend/project/src/main/java/com/wayfarer/project/mapper/ProjectMemberMapper.java
@@ -0,0 +1,15 @@
+package com.wayfarer.project.mapper;
+
+import com.wayfarer.project.dto.ProjectMemberDto;
+import com.wayfarer.project.dto.ProjectMemberResponseDto;
+import com.wayfarer.project.entity.ProjectMember;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProjectMemberMapper {
+    ProjectMember projectMemberDtoToProjectMember(ProjectMemberDto projectMemberDto);
+
+    List<ProjectMemberResponseDto> projectMembersToProjectMemberResponseDtos(List<ProjectMember> projectMembers);
+}
