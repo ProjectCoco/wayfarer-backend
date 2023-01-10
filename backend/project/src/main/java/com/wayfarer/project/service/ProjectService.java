d80545bde05056fd49e31bb4ae57bbc4b6a74c56
minoolian <mw.lee@kakao.com>
2023-01-10 17:21:15 +0900

    feat(project): Skill 조회 추상 메서드 추가
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/project/src/main/java/com/wayfarer/project/service/ProjectService.java
+++ backend/project/src/main/java/com/wayfarer/project/service/ProjectService.java
@@ -1,6 +1,8 @@
 package com.wayfarer.project.service;
 
 import com.wayfarer.project.dto.*;
+import com.wayfarer.project.entity.enummodel.ProjectStatus;
+
 
 public interface ProjectService {
 
@@ -14,6 +16,7 @@ public interface ProjectService {
 
     MultiResponseDto<ProjectArticleResponseDto> readAllProjectArticles(int page, Boolean status);
 
-
     MultiResponseDto<ProjectArticleResponseDto> readProjectArticlesWithTag(int page, String tag, Boolean status);
+
+    MultiResponseDto<ProjectArticleResponseDto> readProjectArticlesWithSkills(int page, SkillParamDto skillParamDto, ProjectStatus status);
 }
