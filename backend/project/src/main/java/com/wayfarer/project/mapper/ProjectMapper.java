e981c9cb144db213c3eeffea517dbcd8ae81b7d0
minoolian <mw.lee@kakao.com>
2023-01-13 15:05:05 +0900

    fix(project): Mapstruct Mapping 수정
    * Member 매핑 추가로 인한 변경사항 수정
    
    Co-authored-by: 5witchkr <hob4419@gmail.com>

--- backend/project/src/main/java/com/wayfarer/project/mapper/ProjectMapper.java
+++ backend/project/src/main/java/com/wayfarer/project/mapper/ProjectMapper.java
@@ -3,6 +3,7 @@ package com.wayfarer.project.mapper;
 import com.wayfarer.project.dto.ProjectArticleDetailResponseDto;
 import com.wayfarer.project.dto.ProjectArticleRequestDto;
 import com.wayfarer.project.dto.ProjectArticleResponseDto;
+import com.wayfarer.project.dto.ProjectMemberResponseDto;
 import com.wayfarer.project.entity.ProjectArticle;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -16,14 +17,14 @@ public interface ProjectMapper {
     @Mapping(source = "startTime", target = "projectTime.startTime")
     ProjectArticle projectRequestDtoToProjectArticle(ProjectArticleRequestDto projectArticleRequestDto);
 
-    @Mapping(source = "projectInfo.status", target = "status")
-    @Mapping(source = "projectTime.startTime", target = "startTime")
-    ProjectArticleResponseDto projectArticleToProjectArticleResponseDto(ProjectArticle projectArticle);
+    @Mapping(source = "projectArticle.projectInfo.status", target = "status")
+    @Mapping(source = "projectArticle.projectTime.startTime", target = "startTime")
+    ProjectArticleResponseDto projectArticleToProjectArticleResponseDto(ProjectArticle projectArticle, List<ProjectMemberResponseDto> projectMemberResponses);
 
-    @Mapping(source = "projectContent.content", target = "content")
-    @Mapping(source = "projectInfo.status", target = "status")
-    @Mapping(source = "projectTime.startTime", target = "startTime")
-    ProjectArticleDetailResponseDto projectArticleToProjectArticleDetailResponseDto(ProjectArticle projectArticle);
+    @Mapping(source = "projectArticle.projectContent.content", target = "content")
+    @Mapping(source = "projectArticle.projectInfo.status", target = "status")
+    @Mapping(source = "projectArticle.projectTime.startTime", target = "startTime")
+    ProjectArticleDetailResponseDto projectArticleToProjectArticleDetailResponseDto(ProjectArticle projectArticle, List<ProjectMemberResponseDto> projectMemberResponses);
 
     List<ProjectArticleResponseDto> projectArticleListToProjectArticleResponseDtoList(List<ProjectArticle> content);
 }
