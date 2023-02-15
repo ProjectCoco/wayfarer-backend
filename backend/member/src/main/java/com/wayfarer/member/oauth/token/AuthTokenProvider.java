96d801c9f9c6d835504aabc7bc1a1d24f824ce41
minoolian <mw.lee@kakao.com>
2023-02-15 18:21:59 +0900

    feat(member): AuthToken 객체 제공 Provider 추가

---
+++ backend/member/src/main/java/com/wayfarer/member/oauth/token/AuthTokenProvider.java
@@ -0,0 +1,63 @@
+package com.wayfarer.member.oauth.token;
+
+
+import com.wayfarer.member.oauth.entity.PrincipalDetails;
+import com.wayfarer.member.oauth.entity.RoleType;
+import com.wayfarer.member.oauth.exception.TokenValidFailedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class AuthTokenProvider {
+
+    private final Key key;
+    private static final String AUTHORITIES_KEY = "role";
+
+    public AuthTokenProvider(String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public AuthToken createAuthToken(String id, String username, Date expiry) {
+        return new AuthToken(id, username, expiry, key);
+    }
+
+    public AuthToken createAuthToken(String id, String username, String role, Date expiry) {
+        return new AuthToken(id, username, role, expiry, key);
+    }
+
+    public AuthToken convertAuthToken(String token) {
+        return new AuthToken(token, key);
+    }
+
+    public Authentication getAuthentication(AuthToken authToken) {
+
+        if(authToken.validate()) {
+
+            Claims claims = authToken.getTokenClaims();
+            String authority = claims.get(AUTHORITIES_KEY).toString();
+            Collection<GrantedAuthority> authorities =
+                    Arrays.stream(new String[]{authority})
+                            .map(SimpleGrantedAuthority::new)
+                            .collect(Collectors.toList());
+
+            log.debug("claims subject := [{}]", claims.getSubject());
+            UserDetails userDetails = new PrincipalDetails(claims.getSubject(), RoleType.of(authority), authorities);
+
+            return new UsernamePasswordAuthenticationToken(userDetails, authToken, authorities);
+        } else {
+            throw new TokenValidFailedException();
+        }
+    }
+}
