7a088de4960e5cddb5c1b402076fd960b911a972
minoolian <mw.lee@kakao.com>
2023-03-06 22:04:22 +0900

    feat(community): 답댓글 필드간 Mapping 선언

--- backend/community/src/main/java/com/wayfarer/community/mapper/ReplyCommentMapper.java
+++ backend/community/src/main/java/com/wayfarer/community/mapper/ReplyCommentMapper.java
@@ -4,9 +4,13 @@ import com.wayfarer.community.dto.replycomment.ReplyCommentRequestDto;
 import com.wayfarer.community.entity.CommunityComment;
 import com.wayfarer.community.entity.ReplyComment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReplyCommentMapper {
 
+    @Mapping(source = "replyCommentRequestDto.content", target = "content")
+    @Mapping(source = "communityComment", target = "communityComment")
     ReplyComment replyCommentRequestDtoToReplyComment(ReplyCommentRequestDto replyCommentRequestDto, CommunityComment communityComment);
+
 }
