e864eb9b37ffbb447379e4f48f4d1da51915f3d8
minoolian <mw.lee@kakao.com>
2023-03-06 23:12:56 +0900

    feat(community): Mapstruct 변환을 위한 Setter 추가

--- backend/community/src/main/java/com/wayfarer/community/dto/article/CommunityArticleDetailResponseDto.java
+++ backend/community/src/main/java/com/wayfarer/community/dto/article/CommunityArticleDetailResponseDto.java
@@ -1,11 +1,13 @@
 package com.wayfarer.community.dto.article;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@Setter
 public class CommunityArticleDetailResponseDto {
 
     private String title;
