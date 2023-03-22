d2ca8e3175a045b7111da138c11e7f0fce3c315a
minoolian <mw.lee@kakao.com>
2023-03-22 12:03:22 +0900

    refactor(community): Service 내 MultiResponseDto 패키지 변경

--- backend/community/src/main/java/com/wayfarer/community/service/article/CommunityService.java
+++ backend/community/src/main/java/com/wayfarer/community/service/article/CommunityService.java
@@ -1,6 +1,6 @@
 package com.wayfarer.community.service.article;
 
-import com.wayfarer.community.dto.*;
+import com.wayfarer.common.dto.MultiResponseDto;
 import com.wayfarer.community.dto.article.CommunityArticleDetailResponseDto;
 import com.wayfarer.community.dto.article.CommunityArticleRequestDto;
 import com.wayfarer.community.dto.article.CommunityArticleResponseDto;
