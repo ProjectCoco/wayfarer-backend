2db6eb0ae24132b5e189bd88c7b42781108280c9
minoolian <mw.lee@kakao.com>
2023-04-05 22:06:49 +0900

    feat(project): Skill 필드 Column 제약 추가

--- backend/project/src/main/java/com/wayfarer/project/entity/vo/ProjectSkill.java
+++ backend/project/src/main/java/com/wayfarer/project/entity/vo/ProjectSkill.java
@@ -3,6 +3,7 @@ package com.wayfarer.project.entity.vo;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Builder
@@ -12,6 +13,8 @@ import javax.persistence.Embeddable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProjectSkill {
+
+    @Column(nullable = false)
     private Long skillId;
 
 }
