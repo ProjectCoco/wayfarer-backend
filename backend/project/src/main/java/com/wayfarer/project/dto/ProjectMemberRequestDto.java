95acbc151e9c8ad1471d2a6668103664d796bcb9
minoolian <mw.lee@kakao.com>
2023-04-10 19:23:49 +0900

    feat(project): Member Request DTO Validation 추가

--- backend/project/src/main/java/com/wayfarer/project/dto/ProjectMemberRequestDto.java
+++ backend/project/src/main/java/com/wayfarer/project/dto/ProjectMemberRequestDto.java
@@ -3,9 +3,14 @@ package com.wayfarer.project.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Positive;
+
 @Getter
 @Setter
 public class ProjectMemberRequestDto {
+
     private String position;
+
+    @Positive
     private int totalMember;
 }
