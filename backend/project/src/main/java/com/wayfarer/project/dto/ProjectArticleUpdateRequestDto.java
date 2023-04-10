9fc1436f86eded9c3e12a0cdc0355bb84ed44222
minoolian <mw.lee@kakao.com>
2023-04-10 19:23:45 +0900

    feat(project): Update DTO Validation 추가

--- backend/project/src/main/java/com/wayfarer/project/dto/ProjectArticleUpdateRequestDto.java
+++ backend/project/src/main/java/com/wayfarer/project/dto/ProjectArticleUpdateRequestDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Setter
@@ -13,11 +14,19 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProjectArticleUpdateRequestDto {
+
+    @NotBlank(message = "제목은 하나 이상의 공백이 아닌 문자를 입력해야 합니다.")
     private String title;
+
     private String projectTags;
+
     private Long projectTotalMember;
+
     private String projectSkills;
+
     private LocalDateTime startTime;
+
+    @NotBlank(message = "본문은 하나 이상의 공백이 아닌 문자를 입력해야 합니다.")
     private String projectContent;
 
 }
