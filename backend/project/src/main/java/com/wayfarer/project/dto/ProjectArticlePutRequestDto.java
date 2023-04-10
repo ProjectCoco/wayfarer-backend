cf50028b950f129589e8201da9e633e1a9cf6fcc
minoolian <mw.lee@kakao.com>
2023-04-10 19:22:02 +0900

    feat(project): Put DTO Validation 추가

--- backend/project/src/main/java/com/wayfarer/project/dto/ProjectArticlePutRequestDto.java
+++ backend/project/src/main/java/com/wayfarer/project/dto/ProjectArticlePutRequestDto.java
@@ -4,16 +4,25 @@ import com.wayfarer.project.entity.enummodel.ProjectSkillEnum;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
 public class ProjectArticlePutRequestDto {
+
+    @NotBlank(message = "제목은 하나 이상의 공백이 아닌 문자를 입력해야 합니다.")
     private String title;
+
     private List<String> projectTags;
+
     private List<ProjectSkillEnum> projectSkills;
+
     private LocalDateTime startTime;
+
+    @NotBlank(message = "본문은 하나 이상의 공백이 아닌 문자를 입력해야 합니다.")
     private String projectContent;
+
     private List<ProjectMemberUpdateDto> projectMembers;
 }
