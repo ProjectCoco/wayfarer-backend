135597bd2951563e8c6d6f24e21c862fc3c423b9
minoolian <mw.lee@kakao.com>
2023-04-12 10:13:21 +0900

    feat(study): Update DTO Validation 추가

--- backend/study/src/main/java/com/wayfarer/study/dto/StudyArticleUpdateRequestDto.java
+++ backend/study/src/main/java/com/wayfarer/study/dto/StudyArticleUpdateRequestDto.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,13 +20,25 @@ import java.util.List;
 public class StudyArticleUpdateRequestDto {
 
     private String target;
+
+    @NotBlank(message = "제목은 하나 이상의 공백이 아닌 문자를 입력해야 합니다.")
     private String title;
+
+    @Positive
     private Long totalMember;
+
+    @Min(0)
     private Long countMember;
+
+    @NotBlank(message = "본문은 하나 이상의 공백이 아닌 문자를 입력해야 합니다.")
     private String content;
+
     private String positionName;
+
     private List<String> studyTags;
+
     private LocalDateTime deadLine;
+
     private StudyStatus active;
 
 }
