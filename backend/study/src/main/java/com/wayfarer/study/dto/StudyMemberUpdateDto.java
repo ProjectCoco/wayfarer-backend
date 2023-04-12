c81f8fc489d61df33410eb39f76bf58d5bffaefb
minoolian <mw.lee@kakao.com>
2023-04-12 10:14:32 +0900

    fix(study): Member Update DTO Validation 추가

--- backend/study/src/main/java/com/wayfarer/study/dto/StudyMemberUpdateDto.java
+++ backend/study/src/main/java/com/wayfarer/study/dto/StudyMemberUpdateDto.java
@@ -3,14 +3,21 @@ package com.wayfarer.study.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+
 @Getter
 @Setter
 public class StudyMemberUpdateDto {
+
     private Long studyMemberId;
 
     private String position;
 
+    @Positive
     private int totalMember;
 
+    @Min(0)
     private int countMember;
+
 }
