f40fc0e9b5e0e19ab36873222917437baa18c456
minoolian <mw.lee@kakao.com>
2023-04-12 10:13:48 +0900

    feat(study): Member Request DTO Validation 추가

--- backend/study/src/main/java/com/wayfarer/study/dto/StudyMemberRequestDto.java
+++ backend/study/src/main/java/com/wayfarer/study/dto/StudyMemberRequestDto.java
@@ -5,10 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Positive;
+
 @Setter
 @Getter
 @NoArgsConstructor
 public class StudyMemberRequestDto {
+
     private String position;
+
+    @Positive
     private int totalMember;
+
 }
