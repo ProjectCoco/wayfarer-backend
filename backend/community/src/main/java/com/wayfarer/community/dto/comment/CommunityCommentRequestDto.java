e1abbdbfc09615dcfc42c8f7dbb1e4a7aa5c90fa
minoolian <mw.lee@kakao.com>
2023-04-13 10:04:29 +0900

    feat(community): Comment Request DTO Validation 추가

--- backend/community/src/main/java/com/wayfarer/community/dto/comment/CommunityCommentRequestDto.java
+++ backend/community/src/main/java/com/wayfarer/community/dto/comment/CommunityCommentRequestDto.java
@@ -4,9 +4,13 @@ package com.wayfarer.community.dto.comment;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class CommunityCommentRequestDto {
 
+    @NotBlank(message = "본문은 하나 이상의 공백이 아닌 문자를 입력해야 합니다.")
     private String content;
+
 }
